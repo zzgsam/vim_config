7c9f3e84d3c042b091d87d2791f1d41f2acbab70
zzgsam <zzgsam@126.com>
2019-04-24 01:14:06 +0200

    YCM added

--- config.sh
+++ config.sh
@@ -7,3 +7,14 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 #install pydiction
 cd ~/.vim/bundle
 git clone http://github.com/rkulla/pydiction.git
+
+#install youcompleteme
+sudo apt-get install build-essential
+sudo apt-get install cmake
+sudo apt-get install pyhton-dev python2.7-dev python3-dev
+cd ~/.vim/bundle
+git clone https://github.com/Valloric/YouCompleteme.git
+cd YouCompleteMe
+git submodule update --init --recursive
+./install.py --clang-completer
+
