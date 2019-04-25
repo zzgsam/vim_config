1a37f6ae209630d2e10c8bdc6dcaf66cff2fa221
zzgsam <32685391+zzgsam@users.noreply.github.com>
2019-04-25 13:46:33 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -14,7 +14,7 @@ sudo apt-get install cmake
 sudo apt-get install pyhton-dev python2.7-dev python3-dev
 cd ~/.vim/bundle
 git clone https://github.com/Valloric/YouCompleteme.git
-cd YouCompleteMe
+cd Youcompleteme
 git submodule update --init --recursive
 ./install.py --clang-completer
 
